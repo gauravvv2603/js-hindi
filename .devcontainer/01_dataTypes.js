a8ee699a655bfe726c201e485e3c8db2457a6959
Gaurav <gauravvv2603@gmail.com>
2026-06-23 17:08:04 +0000

    DataTypes

---
+++ .devcontainer/01_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) // we are using nodeJs , not browser
+
+console.log(3 + 3);
+
+console.log("gaurav");
+
+
+ let name = "gaurav"
+ let age = 18
+ let isLoggedIn = false
+ let state;
+
+
+// DATA TYPES 
+ // number => 2 to power 53 
+ // bigint 
+ // string => "" , ''
+ // boolean => true/false
+ // null => standaLone value
+ // unedfined =>
+// symbols => unique 
+
+
+
+// object
+
+
+console.log( typeof "gaurav");
+console.log(typeof age ); 
+console.log(typeof null); //object
+console.log(typeof undefineed) // undefined
+
+

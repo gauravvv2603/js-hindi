1f1a30aba6b22e75fe2d683d7e6d93e215fb6da9
Gaurav <gauravvv2603@gmail.com>
2026-06-23 07:10:45 +0000

    01_variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId= 14453
+let accountEmail= "gaurav@gmail.con"
+var accountPassword = "1234"
+accountCity = " north west Delhi"
+let accountState; //undefined value
+  
+ //accountId = 2 // NOT ALLOWED
+
+
+console.log(accountId);
+
+accountEmail="h4554y@gmail.com"
+accountPassword="245432"
+accountCity="bengluru"
+  /*
+      prefer not to use var 
+      becouse of issues in block scope and functional scope
+*/
+
+console.log([accountId, accountEmail,accountPassword,accountCity,accountState]);

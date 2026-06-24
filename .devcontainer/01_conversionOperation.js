11e4122e1cc162788340e5d48fb4d17ed8bad50f
Gaurav <gauravvv2603@gmail.com>
2026-06-24 05:46:40 +0000

    01_conversionOperation

--- .devcontainer/01_conversionOperation.js
+++ .devcontainer/01_conversionOperation.js
@@ -19,6 +19,7 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 // true => 1 ; false => 0
 
+/*
 //let isLoggedIn = 1
 let isLoggedIn = "gaurav"
 
@@ -28,3 +29,57 @@ console.log(boolenIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false 
 // "gaurav" =>  true
+*/
+
+
+
+// ****************************** Operations *****************************
+
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " gaurav"
+
+let str3 = str1 + str2
+
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 +2);
+//console.log(1 + 2 + "2");
+
+
+//console.log( (3+4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);  
+
+
+
+
+// LINK TO STUDY
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+

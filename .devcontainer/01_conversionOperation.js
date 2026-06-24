4b74b2374690f79fcee1ff46707ce97a1472c9b0
Gaurav <gauravvv2603@gmail.com>
2026-06-24 05:14:47 +0000

    01_conversionOperation

---
+++ .devcontainer/01_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = null // 0
+// let score = undefined // undefined
+// let score = true // 1 
+let score = null
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// let valueInNumber = String(score) 
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+//let isLoggedIn = 1
+let isLoggedIn = "gaurav"
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "gaurav" =>  true
